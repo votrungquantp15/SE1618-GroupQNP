0ff8d36c053b9ea5c0d7de9799a7637118a592d4
Quan Vo <votrungquan.tp15@gmail.com>
2022-05-11 07:50:25 +0700

    Update Name

--- src/java/dao/CustomerDAO.java
+++ src/java/dao/CustomerDAO.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dto;
+package dao;
 
 /**
  *
  * @author votru
  */
-public class User {
+public class CustomerDAO {
     
 }
--- src/java/dto/Customer.java
+++ src/java/dto/Customer.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dao;
+package dto;
 
 /**
  *
  * @author votru
  */
-public class UserDAO {
+public class Customer {
     
 }
